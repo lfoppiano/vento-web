25a310aaa2e444df38bfa80b1039499facbf44f9
Luca Foppiano <lfoppiano@bioparco.(none)>
2012-04-08 14:10:36 +0200

    added a better error handling

--- src/main/groovy/org/vento/crawler/processor/TwitterPreprocessor.groovy
+++ src/main/groovy/org/vento/crawler/processor/TwitterPreprocessor.groovy
@@ -14,7 +14,7 @@ public class TwitterPreprocessor implements Processor {
         def resultJson = new JsonSlurper().parseText(json)
 
         def root = new Twits()
-        println exchange.getIn().getMessageId()
+        //println exchange.getIn().getMessageId()
 
         root.twits = new ArrayList<Twit>();
 
--- src/main/java/org/vento/crawler/route/TwitterCrawlerRoute.java
+++ src/main/java/org/vento/crawler/route/TwitterCrawlerRoute.java
@@ -3,6 +3,7 @@ package org.vento.crawler.route;
 import org.apache.camel.Endpoint;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.vento.crawler.processor.TwitterPreprocessor;
 import org.vento.model.Twit;
@@ -24,13 +25,28 @@ public class TwitterCrawlerRoute extends RouteBuilder {
     @EndpointInject(ref = "sourceFileQuery")
     private Endpoint sourceFileQuery;
 
-    @EndpointInject(ref = "queryQueue")
-    private Endpoint queryQueue;
-
     @EndpointInject(ref = "outputDirectory")
     private Endpoint outputDirectory;
 
+    @EndpointInject(ref = "rejectTwitterLocation")
+    private Endpoint twitterRejectEndpoint;
+
     public void configure(){
+
+        errorHandler(
+                deadLetterChannel(twitterRejectEndpoint)
+                        .retryAttemptedLogLevel(LoggingLevel.WARN)
+        );
+
+        onException(java.net.SocketException.class)
+                .retryAttemptedLogLevel(LoggingLevel.WARN)
+                .maximumRedeliveries(5)
+                .redeliveryDelay(5000);
+        
+        onException(org.apache.commons.httpclient.NoHttpResponseException.class)
+                .retryAttemptedLogLevel(LoggingLevel.WARN)
+                .maximumRedeliveries(5)
+                .redeliveryDelay(5000);
 
         TwitterPreprocessor twitterPreprocessor = new TwitterPreprocessor();
         from(sourceFileQuery)
@@ -39,15 +55,15 @@ public class TwitterCrawlerRoute extends RouteBuilder {
                 .split().tokenize("\n")
                 .loop(15).copy()
                 .transform(body().append(simple("&page=${header.CamelLoopIndex}++")))
-                .to(queryQueue);
+                .to("seda:queryQueue");
 
-        from(queryQueue)
+        from("seda:queryQueue?concurrentConsumers=3")
                 .autoStartup(false)
                 .routeId("TwitterCrawler")
                 //.to("log:httpQuery?level=INFO&showHeaders=true")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setHeader(Exchange.HTTP_QUERY, simple("q=${body}&lang=en&rpp=100"))
-                .to("http://search.twitter.com/search.json")
+                .to("http://search.twitter.com/search.json?httpClient.cookiePolicy=ignoreCookies")
                 .convertBodyTo(String.class, "UTF-8")
                 .process(twitterPreprocessor)
                 .split().xpath("//twits/twit").streaming()
