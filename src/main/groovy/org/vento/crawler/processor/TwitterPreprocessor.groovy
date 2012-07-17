7295b4725f93526e8bb4796b32ddd8bd7c2f2d76
lfoppiano <luca@foppiano.org>
2012-07-17 23:59:44 +0200

    removed camel:run as it was not working

--- src/main/groovy/org/vento/crawler/processor/TwitterPreprocessor.groovy
+++ src/main/groovy/org/vento/crawler/processor/TwitterPreprocessor.groovy
@@ -15,7 +15,6 @@ public class TwitterPreprocessor implements Processor {
         def resultJson = new JsonSlurper().parseText(json)
 
         def root = new Twits()
-        //println exchange.getIn().getMessageId()
 
         root.twits = new ArrayList<Twit>();
 
