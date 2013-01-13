55a9297c643f851d5ec65633e812e9d25343d690
lfoppiano <luca@foppiano.org>
2013-01-13 12:04:00 +0100

    changed the textProcessor, the emoticons are now removed in another method that evaluate the positivness or negativness of the text based on the emoticons, before removing them

--- src/main/groovy/org/vento/crawler/twitter/processor/TwitterPreprocessor.groovy
+++ src/main/groovy/org/vento/crawler/twitter/processor/TwitterPreprocessor.groovy
@@ -21,9 +21,11 @@ public class TwitterPreprocessor implements Processor {
 
         resultJson.results.each{ result ->
             def twit = new Twit()
+
+            //collecting
             twit.twitterId = result.id_str
             twit.query = resultJson.query
-            twit.text = textProcessing(result.text)
+            twit.text = result.text
             twit.createdAt = result.created_at
             twit.fromUserIdStr = result.from_user_id_str
             twit.toUserIdStr = result.to_user_id_str
@@ -33,24 +35,74 @@ public class TwitterPreprocessor implements Processor {
             twit.geo =  result.geo
             twit.type = VentoTypes.CLASSIFICATION
             twit.getScoreHistories()
+
+            //cleaning up and enriching
+            twit = analyzeAndCleanEmotions(twit)
+            twit.text = textProcessing(twit.text)
             root.twits.add(twit)
         }
+
+
         exchange.getOut().setBody(root)
+    }
+
+    /**
+     * Analyze and remove emoticons.
+     *  - analyze the positive emoticons, and set referenceScore = 3.0 if any is found
+     *  - analyze the negative emoticons,
+     *      * if no referenceScore has been set, it set one
+     *      * if a positive reference score has been set, it set to 0.0 the result which
+     *          invalidate the calculation, because there is no clear state of emotion
+     *
+     * @param body a twit with at least text != null
+     * @return a twit with the text cleaned and the referenceScore set
+     */
+    private analyzeAndCleanEmotions(Twit body){
+        String positiveRegex =  /([;:8]?[-=]?)[P)}DFf]/
+        String negativeRegex =  /[;:8]?[-=]?[(P){]/
+
+        def text = body?.text
+        def referenceScore = body.referenceScore
+        text = text.replaceAll(positiveRegex) {
+            if(!referenceScore)
+                referenceScore = '3.0'
+
+            return ''
+        }
+
+        text = text.replaceAll(negativeRegex) {
+            if(!referenceScore)
+                referenceScore = '1.0'
+            else if(referenceScore == '3.0')
+                referenceScore = '0.0'
+
+            return ''
+        }
+
+        body.text = text.trim()
+        body.referenceScore = referenceScore == '0.0' ? null : referenceScore
+        return body;
     }
 
     private String textProcessing(String text) {
         //Remove recipients
-        text = text.replaceAll(/@\w+/, '')
-        //Remove tags
-        text = text.replaceAll(/#\w+/, '')
-        //Remove emoticons
-        text = text.replaceAll(/[;:8](-)?[)(P]/, '')
+        text = text.replaceAll(/@\w+(:)?/, '')
+
+        //Remove hash of tags
+        text = text.replaceAll(/#/, '')
+
+        //Remove emoticons neutral
+        //text = text.replaceAll(/[;:8](-)?[(P)}{D]/, '')
+
         //Double space to space
         text = text.replaceAll(/\s\s/, ' ')
+
         //Remove links/urls
-        text = text.replaceAll(/http:\/\/[a-zA-Z0-9\/-=.:]+/, '')
+        text = text.replaceAll(/(via)? http:\/\/[a-zA-Z0-9\/-=.:]+/, '')
+
         //Remove retweets
         text = text.replaceAll(/^?RT\s?:/, '')
+
         //Remove invalid characters
         text = StringProcessor.removeInvalidUtf8Chars(text)
 
--- src/test/groovy/org/vento/crawler/twitter/processor/TwitterPreprocessorTest.groovy
+++ src/test/groovy/org/vento/crawler/twitter/processor/TwitterPreprocessorTest.groovy
@@ -9,26 +9,29 @@ import org.apache.camel.impl.DefaultExchange
 import org.apache.camel.impl.DefaultMessage
 import org.junit.Before
 import org.junit.Test
+import org.vento.model.Twit
 
 public class TwitterPreprocessorTest {
 
     Exchange exchange;
 
+    private TwitterPreprocessor target
+
     @Before
     public void setUp() throws Exception {
+        target = new TwitterPreprocessor()
 
         CamelContext context = new DefaultCamelContext();
         exchange = new DefaultExchange(context);
-
-        String body = getClass().getResourceAsStream('twitter-response-body.json').getText();
-        Message messageIn = new DefaultMessage();
-        messageIn.setBody(body)
-        exchange.setIn(messageIn)
     }
 
     @Test
-    public void testProcess() throws Exception {
-        TwitterPreprocessor target = new TwitterPreprocessor();
+    public void testProcessEnd2End() throws Exception {
+        String bodyIn = getClass().getResourceAsStream('twitter-response-body.json').getText();
+        Message messageIn = new DefaultMessage();
+        messageIn.setBody(bodyIn)
+        exchange.setIn(messageIn)
+
         target.process(exchange);
 
         def body = exchange.getOut().getBody()
@@ -36,6 +39,79 @@ public class TwitterPreprocessorTest {
 
         assert body.twits.size() == 15
         assert body.twits.get(0).twitterId == '123505817893863425'
-        assert body.twits.get(14).text == 'My better is better than your better'
+        assert body.twits.get(14).text == 'My better is better than your better Nike'
+    }
+
+    @Test
+    public void textProcessing() {
+        assert target.textProcessing("\"The Future of Google Maps: You'll Never Be Lost Again\": http://t.co/xXYRSrSi") ==
+                "\"The Future of Google Maps: You'll Never Be Lost Again\":"
+        assert target.textProcessing("@lddio c'è un certo #berlusconi che dopo la recente performance a #serviziopubblico vorrebbe confrontarsi con te...") ==
+                "c'è un certo berlusconi che dopo la recente performance a serviziopubblico vorrebbe confrontarsi con te..."
+    }
+
+    @Test
+    public void testAnalyzeAndCleanEmotionsPositive() {
+        Twit t = new Twit(
+                twitterId: '12345',
+                text: 'bao :) :-)',
+                referenceScore: null
+        )
+
+        Twit result = target.analyzeAndCleanEmotions(t)
+        assert result.referenceScore == '3.0'
+        assert result.text == 'bao'
+    }
+
+    @Test
+    public void testAnalyzeAndCleanEmotionsNegative() {
+        Twit t = new Twit(
+                twitterId: '12345',
+                text: ':( bao ',
+                referenceScore: null
+        )
+
+        Twit result = target.analyzeAndCleanEmotions(t)
+        assert result.referenceScore == '1.0'
+        assert result.text == 'bao'
+    }
+
+    @Test
+    public void testAnalyzeAndCleanEmotionsNegativeDouble() {
+        Twit t = new Twit(
+                twitterId: '12345',
+                text: ':( bao :-(',
+                referenceScore: null
+        )
+
+        Twit result = target.analyzeAndCleanEmotions(t)
+        assert result.referenceScore == '1.0'
+        assert result.text == 'bao'
+    }
+
+    @Test
+    public void testAnalyzeAndCleanEmotionsRemoveSpaces() {
+        Twit t = new Twit(
+                twitterId: '12345',
+                text: 'Ciao :) come :D va a tutti un :-F bao :) :-)',
+                referenceScore: null
+        )
+
+        Twit result = target.analyzeAndCleanEmotions(t)
+        assert result.text == 'Ciao  come  va a tutti un  bao'
+        assert result.referenceScore == '3.0'
+    }
+
+    @Test
+    public void testAnalyzeAndCleanEmotionsMixed() {
+        Twit t = new Twit(
+                twitterId: '12345',
+                text: 'Ciao :( come :( va a tutti un :-F bao :) :-)',
+                referenceScore: null
+        )
+
+        Twit result = target.analyzeAndCleanEmotions(t)
+        assert result.text == 'Ciao  come  va a tutti un  bao'
+        assert result.referenceScore == null
     }
 }
